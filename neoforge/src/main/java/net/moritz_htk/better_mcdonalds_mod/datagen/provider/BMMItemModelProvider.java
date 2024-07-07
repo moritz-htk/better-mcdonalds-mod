dc176b27767d2ad5432fff32a090e8314e9d8410
simon-mc28 <hartkopfmoritz5@gmail.com>
2024-07-07 12:37:21 +0200

    Removed knife

--- neoforge/src/main/java/net/moritz_htk/better_mcdonalds_mod/datagen/provider/BMMItemModelProvider.java
+++ neoforge/src/main/java/net/moritz_htk/better_mcdonalds_mod/datagen/provider/BMMItemModelProvider.java
@@ -1,17 +1,12 @@
 package net.moritz_htk.better_mcdonalds_mod.datagen.provider;
 
 import dev.architectury.registry.registries.RegistrySupplier;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.moritz_htk.better_mcdonalds_mod.BetterMcDonaldsMod;
 import net.moritz_htk.better_mcdonalds_mod.registry.BMMItems;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
-import net.neoforged.neoforge.client.model.generators.ModelFile;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
-
-import java.util.Objects;
 
 public class BMMItemModelProvider extends ItemModelProvider {
     public BMMItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
@@ -23,11 +18,5 @@ public class BMMItemModelProvider extends ItemModelProvider {
         for (RegistrySupplier<Item> item : BMMItems.ITEMS) {
             basicItem(item.get());
         }
-        handheldItem(BMMItems.KNIFE.get());
-    }
-
-    public void handheldItem(Item item) {
-        ResourceLocation location = Objects.requireNonNull(BuiltInRegistries.ITEM.getKey(item));
-        getBuilder(location.toString()).parent(new ModelFile.UncheckedModelFile("item/handheld")).texture("layer0", ResourceLocation.fromNamespaceAndPath(location.getNamespace(), "item/" + location.getPath()));
     }
 }
