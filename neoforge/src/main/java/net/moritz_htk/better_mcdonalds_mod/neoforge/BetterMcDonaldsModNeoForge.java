76d8e754e4ddd7ffcac03dd4ec382367c6492199
moritz-htk <hartkopfmoritz5@gmail.com>
2025-01-02 10:44:51 +0100

    Removed no longer needed NeoForge classes

--- neoforge/src/main/java/net/moritz_htk/better_mcdonalds_mod/neoforge/BetterMcDonaldsModNeoForge.java
+++ neoforge/src/main/java/net/moritz_htk/better_mcdonalds_mod/neoforge/BetterMcDonaldsModNeoForge.java
@@ -1,14 +1,11 @@
 package net.moritz_htk.better_mcdonalds_mod.neoforge;
 
 import net.moritz_htk.better_mcdonalds_mod.BetterMcDonaldsMod;
-import net.moritz_htk.better_mcdonalds_mod.neoforge.loot.BMMLootModifiers;
-import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 @Mod(BetterMcDonaldsMod.MOD_ID)
 public class BetterMcDonaldsModNeoForge {
-    public BetterMcDonaldsModNeoForge(IEventBus modEventBus) {
+    public BetterMcDonaldsModNeoForge() {
         BetterMcDonaldsMod.init();
-        BMMLootModifiers.LOOT_MODIFIER_SERIALIZERS.register(modEventBus);
     }
 }
