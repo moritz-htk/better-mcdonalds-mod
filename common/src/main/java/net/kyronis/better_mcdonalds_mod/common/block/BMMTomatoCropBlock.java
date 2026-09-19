ad5e755c9333d54bbfce0a081c12579c92353d27
Kyronis <info@kyronis.net>
2026-09-19 20:53:59 +0200

    Add custom voxel shapes for tomato and lettuce crop growth stages

--- common/src/main/java/net/kyronis/better_mcdonalds_mod/common/block/BMMTomatoCropBlock.java
+++ common/src/main/java/net/kyronis/better_mcdonalds_mod/common/block/BMMTomatoCropBlock.java
@@ -1,6 +1,8 @@
 package net.kyronis.better_mcdonalds_mod.common.block;
 
 import net.kyronis.better_mcdonalds_mod.common.registry.BMMItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
@@ -11,10 +13,18 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class BMMTomatoCropBlock extends CropBlock {
     public static final int MAX_AGE = 3;
     public static final IntegerProperty AGE = BlockStateProperties.AGE_3;
+    private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D,  4.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D,  8.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D,  12.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D,  16.0D, 16.0D)
+    };
 
     public BMMTomatoCropBlock(Properties properties) {
         super(properties.mapColor(MapColor.PLANT).noCollision().randomTicks().instabreak().sound(SoundType.CROP).pushReaction(PushReaction.POPPED).noOcclusion());
@@ -33,6 +43,11 @@ public class BMMTomatoCropBlock extends CropBlock {
     @Override
     public int getMaxAge() {
         return MAX_AGE;
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE_BY_AGE[state.getValue(this.getAgeProperty())];
     }
 
     @Override
